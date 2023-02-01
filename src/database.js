0c9ab995e7ee85a24a36d76546ed46b032eb8adc
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-01-31 21:59:18 -0300

    Fim do desafio 01-fundamentos NodeJs 2023

---
+++ src/database.js
@@ -0,0 +1,71 @@
+import fs from 'node:fs/promises';
+import * as importCsv from './streams/import-csv.js';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(() => [
+        importCsv(),
+      ])
+      .then(data => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  };
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2));
+  };
+
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          if (!value) return true
+
+          return row[key].includes(value);
+        });
+      });
+    }
+    return data;
+  };
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  };
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  };
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex > -1) {
+      const row = this.#database[table][rowIndex];
+      this.#database[table][rowIndex] = { id, ...row, ...data }
+      this.#persist();
+    }
+  };
+}
